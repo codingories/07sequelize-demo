747620f3d58b8931e5b4a41556f68984a878f37f
朱骏杰 <ories.zhu@mochain.vip>
2020-06-24 20:55:47 +0800

    实现用sequelize增加数据

---
+++ test.js
@@ -0,0 +1,28 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = new Sequelize("fang", "root", "123456", {
+  dialect: "mysql",
+});
+// 创建了user模型
+class User extends Model {}
+// 初始化User
+User.init(
+  {
+    username: DataTypes.STRING,
+    birthday: DataTypes.DATE,
+  },
+  { sequelize, modelName: "user" }
+);
+// 同步到数据库
+sequelize
+  // 创建一条记录
+  .sync() // 同步
+  .then(() =>
+    User.create({
+      username: "janedoe",
+      birthday: new Date(1980, 6, 20),
+    })
+  )
+  // 打印结果
+  .then((jane) => {
+    console.log(jane.toJSON());
+  });
